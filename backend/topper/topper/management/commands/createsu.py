a06946330bcce490fa6c654da347e985a1e0a4be
Vik Sunkavalli <vsunkavalli@gmail.com>
2018-12-01 17:36:19 -0500

    Fix createsu to use general settings

--- backend/topper/topper/management/commands/createsu.py
+++ backend/topper/topper/management/commands/createsu.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from topper.settings import SUPERUSER
+from django.conf import settings
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(SUPERUSER['name'], SUPERUSER['email'], SUPERUSER['password'])
+            User.objects.create_superuser(settings.SUPERUSER['name'], settings.SUPERUSER['email'], settings.SUPERUSER['password'])
