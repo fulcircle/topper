411ec593ce8603bd41152088fc9af78a102d52de
Vik Sunkavalli <vsunkavalli@gmail.com>
2018-12-02 00:50:24 -0500

    Update settings for beanstalk to run celery

--- backend/topper/topper/admin.py
+++ backend/topper/topper/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from topper.models import Service, Story, Podcast
 
 # Register your models here.
+
+admin.site.register(Service)
+admin.site.register(Story)
+admin.site.register(Podcast)
